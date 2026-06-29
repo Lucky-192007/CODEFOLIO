af8200179381aedf19cd8e29f9339651bfb61a08
Lucky-192007 <Lucky29639@gmail.com>
2026-06-29 23:19:03 +0530

    changing my axios instance according to the auth context

--- codefolio/src/services/api.js
+++ codefolio/src/services/api.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_API
 });
 
 API.interceptors.request.use((config) => {
-  const user = JSON.parse(
-    localStorage.getItem("dashboard_user")
-  );
 
-  if (user?.token) {
-    config.headers.Authorization = `Bearer ${user.token}`;
-  }
+    const token = localStorage.getItem("token");
 
-  return config;
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
 });
 
 export default API;
